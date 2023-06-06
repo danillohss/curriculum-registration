d65856f768358e03eac0d58dedbb34a64e6aead8
Danillo Soares <danillohss@gmail.com>
2023-06-06 02:20:01 -0300

    trocando element-ui pelo bootstrap e adc o nav-bar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="home">
-    <el-button color="#626aef" :dark="isDark" plain>Plain</el-button>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Menu</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link" href="#">Cadastrar Currículo</a>
+            <a class="nav-link" href="#">Meus Currículos</a>
+          </div>
+        </div>
+      </div>
+    </nav>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: "HomeView",
   components: {},
