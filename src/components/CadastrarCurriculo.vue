620cd5c985ad80f6fffe63a13ea1fba101eb2866
Danillo Soares <danillohss@gmail.com>
2023-07-14 02:08:36 -0300

    Configurando formulario + mascaras de input

--- src/components/CadastrarCurriculo.vue
+++ src/components/CadastrarCurriculo.vue
@@ -4,6 +4,8 @@
     <div class="row">
       <div class="col">
         <h4>Cadastre seu currículo gratuitamente!</h4>
+        <br />
+        <h5>Dados pessoais</h5>
       </div>
     </div>
     <div class="row mt-3">
@@ -23,7 +25,7 @@
       </div>
       <div class="col">
         <label class="form-label">Telefone Celular</label>
-        <input type="text" class="form-control" />
+        <input type="text" class="form-control" v-maska="'(##) # ####-####'" />
       </div>
       <div class="col">
         <label class="form-label">LinkedIn</label>
@@ -34,25 +36,65 @@
         <input type="text" class="form-control" />
       </div>
     </div>
+    <br />
+    <h5>Experiência</h5>
     <div class="row mt-3">
       <div class="col">
-        <label class="form-label">Remuneração</label>
+        <label class="form-label">Nome da Empresa</label>
+        <input type="text" class="form-control" />
+      </div>
+      <div class="col">
+        <label class="form-label">Cargo ocupado</label>
+        <input type="text" class="form-control" />
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col">
+        <label for="startDate">Inicio</label>
+        <input
+          id="startDate"
+          class="form-control"
+          type="date"
+          v-model="dataInicio"
+        />
+      </div>
+      <div class="col">
+        <label for="startDate">Fim</label>
+        <input
+          id="startDate"
+          class="form-control"
+          type="date"
+          v-model="dataFim"
+          :disabled="checkBoxAtual"
+        />
+      </div>
+      <div class="col">
+        <div class="form-check">
+          <br />
+          <label class="form-check-label"> Atualmente trabalho aqui </label>
+          <input
+            class="form-check-input"
+            type="checkbox"
+            v-model="checkBoxAtual"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col">
+        <label class="form-label">Ultima remuneração</label>
         <div class="input-group mb-3">
           <div class="input-group-prepend">
             <span class="input-group-text">R$</span>
           </div>
-          <input
-            type="text"
-            class="form-control"
-            v-model="salario"
-            v-maska="'#,###.##'"
-          />
+          <input type="text" class="form-control" v-maska="'#,###.##'" />
         </div>
         <div class="form-text">Informe a Remuneração</div>
       </div>
       <div class="col">
         <label class="form-label">Tipo de contratação</label>
-        <select class="form-select" v-model="tipo">
+        <select class="form-select">
           <option value="1">PJ</option>
           <option value="2">CLT</option>
         </select>
@@ -60,7 +102,7 @@
       </div>
       <div class="col">
         <label class="form-label">Modalidade</label>
-        <select class="form-select" v-model="modalidade">
+        <select class="form-select">
           <option value="1">Home Office</option>
           <option value="2">Híbrido</option>
           <option value="3">Presencial</option>
@@ -70,9 +112,7 @@
     </div>
     <div class="row mt-3">
       <div class="col">
-        <button type="submit" class="btn btn-primary" @click="salvarVaga()">
-          Cadastrar
-        </button>
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
       </div>
     </div>
   </div>
@@ -83,5 +123,16 @@ import Topo from "../components/Topo.vue";
 export default {
   name: "CADASTRARCURRICULO",
   components: { Topo },
+  data: () => ({
+    checkBoxAtual: false,
+    dataFim: null,
+  }),
+  watch: {
+    checkBoxAtual() {
+      if (this.checkBoxAtual) {
+        this.dataFim = null;
+      }
+    },
+  },
 };
 </script>
